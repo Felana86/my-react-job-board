8debac2fc17dcd06e04b713837773d3bfaa5a283
CaroWiese <wiese.caro@gmail.com>
2021-08-02 00:20:01 +0200

    Try cooldown example (commented)

--- back/app/Discord/commands/ping.js
+++ back/app/Discord/commands/ping.js
@@ -1,5 +1,6 @@
 module.exports = {
 	name: 'ping',
+	cooldown: 10,
 	description: 'Ping!',
 	execute(message) {
 		message.channel.send('Pong.');
--- back/app/Discord/discordController.js
+++ back/app/Discord/discordController.js
@@ -11,6 +11,7 @@ const discordController = {
 
             const client = new Discord.Client();
             client.commands = new Discord.Collection();
+            // client.cooldowns = new Discord.Collection();
 
             const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
 
@@ -18,6 +19,34 @@ const discordController = {
                 const command = require(__dirname + `/commands/${file}`);
                 client.commands.set(command.name, command);
             };
+
+            // const { cooldowns } = client;
+
+            // if (!cooldowns.has(command.name)) {
+            //     cooldowns.set(command.name, new Discord.Collection());
+            // }
+
+            // const now = Date.now();
+            // const timestamps = cooldowns.get(command.name);
+            // const cooldownAmount = (command.cooldown || 3) * 1000;
+
+            // if (timestamps.has(message.author.id)) {
+            //     const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+            //     if (now < expirationTime) {
+            //         const timeLeft = (expirationTime - now) / 1000;
+            //         return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+            //     }
+            // }
+
+            // timestamps.set(message.author.id, now);
+            // setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+            // try {
+            //     // ...
+            // } catch (error) {
+            //     // ...
+            // }
 
             // when the client is ready, run this code
             // this event will only trigger one time after logging in
